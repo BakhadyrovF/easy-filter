ec1fbd9feb620bfdee4faa5b00791ea2bd3e9eaf
BakhadyrovF <BakhadyrovF@gmail.com>
2022-07-10 21:03:15 +0300

    added gitignore

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,11 +6,11 @@
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'reference' => NULL,
-        'name' => 'bakhadyrovf/easy-filter',
+        'name' => 'bakhadyrovf/package',
         'dev' => true,
     ),
     'versions' => array(
-        'bakhadyrovf/easy-filter' => array(
+        'bakhadyrovf/package' => array(
             'pretty_version' => '1.0.0+no-version-set',
             'version' => '1.0.0.0',
             'type' => 'library',
