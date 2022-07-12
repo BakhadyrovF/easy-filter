e33f07e5497e8d76f5492a08c90a6a9264848735
BakhadyrovF <BakhadyrovF@gmail.com>
2022-07-12 17:29:31 +0300

    fix: bug with publishing configuration from invalid directory

--- src/Providers/QueryFilterProvider.php
+++ src/Providers/QueryFilterProvider.php
@@ -10,13 +10,13 @@ class QueryFilterProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([__DIR__ . '/../config/easy-filter.php' => config_path('easy-filter.php')], 'easy-filter-config');
+            $this->publishes([__DIR__ . '/../../config/easy-filter.php' => config_path('easy-filter.php')], 'easy-filter-config');
         }
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/easy-filter.php', 'easy-filter');
+        $this->mergeConfigFrom(__DIR__ . '/../../config/easy-filter.php', 'easy-filter');
 
         if ($this->app->runningInConsole()){
             $this->commands(MakeFilterClass::class);
