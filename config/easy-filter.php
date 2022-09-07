a95caae28653eadf7cd0f9a678edc953667f14bf
BakhadyrovF <BakhadyrovF@gmail.com>
2022-09-07 18:26:24 +0500

    ref: config

--- config/easy-filter.php
+++ config/easy-filter.php
@@ -3,12 +3,6 @@
 return [
 
     /**
-     * Query parameter by which will take filterables.
-     * Default: filters
-     */
-    'base-parameter' => 'filters',
-
-    /**
      * Base folder's name in app directory.
      * Default: Filters
      */
